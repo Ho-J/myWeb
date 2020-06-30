02c46d2d1f09efe4ee816c7f390f8baa26730000
Ho-J <hoj31415@163.com>
2020-06-30 19:05:08 +0800

    Signed-off-by: Ho-J <hoj31415@163.com>

---
+++ js/RegisterLogin.js
@@ -0,0 +1,11 @@
+
+$(function(){
+	//登录切换操作
+	document.querySelector('.img__btn').addEventListener('click', function() {
+	    document.querySelector('.dowebok').classList.toggle('s--signup')
+	});
+	
+	//登录验证
+	
+	//注册验证
+})
